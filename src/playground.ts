c379f706c294b69efdeae4125414c3b8e505ae19
Edem Agbenyo <edem.agbenyo@gmail.com>
2023-06-24 11:10:01 +0000

    enable declaration

--- src/playground.ts
+++ src/playground.ts
@@ -1,9 +1,9 @@
-import StorageWrapper from "../src/index";
+// import StorageWrapper from "ts-storage-wrapper";
 
-StorageWrapper.setItem("name", "John Doe");
+// StorageWrapper.setItem("name", "John Doe");
 
-// Storing an object with a specific key
-const user = { name: "John Doe", age: 25 };
-StorageWrapper.setItem("user", user);
+// // Storing an object with a specific key
+// const user = { name: "John Doe", age: 25 };
+// StorageWrapper.setItem("user", user);
 
-// const
+// // const
